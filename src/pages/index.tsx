445cc19eed444e19a0a441c7b4fe04cd4895e7da
snakydh <skdanielg5@gmail.com>
2023-04-14 20:04:07 -0500

    Test Discord Bot

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,12 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between p-24">
       <h1>Condorcito</h1>
+      <p className="text-emerald-600">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat, cumque
+        nihil sapiente at reiciendis dolorum? Ipsam, delectus. Necessitatibus
+        ipsam dolore reiciendis unde repudiandae. Sequi ex, repellat doloribus
+        sed aliquid rem?
+      </p>
     </main>
-  )
+  );
 }
